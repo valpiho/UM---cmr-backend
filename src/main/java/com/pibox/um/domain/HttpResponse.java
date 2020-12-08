6284ce70445fcec44a667ee768134ead57e39a21
Valentin Piho <val.piho@gmail.com>
2020-12-08 22:24:49 +0200

    Adding date field and constructor

--- src/main/java/com/pibox/um/domain/HttpResponse.java
+++ src/main/java/com/pibox/um/domain/HttpResponse.java
@@ -1,16 +1,27 @@
 package com.pibox.um.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import org.springframework.http.HttpStatus;
+import java.util.Date;
 
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class HttpResponse {
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy hh:mm:ss", timezone = "EET")
+    private Date timeStamp;
     private int httpStatusCode;
     private HttpStatus httpStatus;
     private String reason;
     private String message;
+
+    public HttpResponse(int httpStatusCode, HttpStatus httpStatus, String reason, String message) {
+        this.timeStamp = new Date();
+        this.httpStatusCode = httpStatusCode;
+        this.httpStatus = httpStatus;
+        this.reason = reason;
+        this.message = message;
+    }
 }
