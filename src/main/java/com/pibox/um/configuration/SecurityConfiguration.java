68c1111bd22a208d1f5f57c7afdb16c17d14ff9b
Valentin Piho <val.piho@gmail.com>
2020-12-08 22:26:28 +0200

    Security configuration

---
+++ src/main/java/com/pibox/um/configuration/SecurityConfiguration.java
@@ -0,0 +1,70 @@
+package com.pibox.um.configuration;
+
+import com.pibox.um.filter.JWTAccessDeniedHandler;
+import com.pibox.um.filter.JWTAuthenticationEntryPoint;
+import com.pibox.um.filter.JWTAuthorizationFilter;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static com.pibox.um.constant.SecurityConstant.PUBLIC_URLS;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final JWTAuthorizationFilter jwtAuthorizationFilter;
+    private final JWTAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JWTAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final UserDetailsService userDetailsService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public SecurityConfiguration(JWTAuthorizationFilter jwtAuthorizationFilter,
+                                 JWTAccessDeniedHandler jwtAccessDeniedHandler,
+                                 JWTAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+                                 @Qualifier("userDetailsService") UserDetailsService userDetailsService,
+                                 BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.jwtAuthorizationFilter = jwtAuthorizationFilter;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.userDetailsService = userDetailsService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors()
+                .and()
+                    .sessionManagement().sessionCreationPolicy(STATELESS)
+                .and()
+                    .authorizeRequests().antMatchers(PUBLIC_URLS).permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                    .exceptionHandling().accessDeniedHandler(jwtAccessDeniedHandler)
+                    .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .and()
+                    .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
