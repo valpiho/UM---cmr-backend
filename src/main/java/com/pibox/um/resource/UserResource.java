b1a86cba630a42bd0ef45aad2af28d64586a5ceb
Valentin Piho <val.piho@gmail.com>
2020-12-14 17:18:40 +0200

    Updating path

--- src/main/java/com/pibox/um/resource/UserResource.java
+++ src/main/java/com/pibox/um/resource/UserResource.java
@@ -1,12 +1,13 @@
 package com.pibox.um.resource;
 
+import com.pibox.um.exception.ExceptionHandling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/user")
-public class UserResource {
+@RequestMapping({"/user", "/"})
+public class UserResource extends ExceptionHandling {
 
     @GetMapping("/home")
     public String showUser() {
