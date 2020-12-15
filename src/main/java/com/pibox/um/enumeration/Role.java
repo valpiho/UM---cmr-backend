990254a04a4c3708f49a44e1eabc3bdc17fbe97d
Valentin Piho <val.piho@gmail.com>
2020-12-15 22:12:07 +0200

    Roles

---
+++ src/main/java/com/pibox/um/enumeration/Role.java
@@ -0,0 +1,22 @@
+package com.pibox.um.enumeration;
+
+import static com.pibox.um.constant.Authority.*;
+
+public enum Role {
+
+    ROLE_USER(USER_AUTHORITIES),
+    ROLE_HR(HR_AUTHORITIES),
+    ROLE_MANAGER(MANAGER_AUTHORITIES),
+    ROLE_ADMIN(ADMIN_AUTHORITIES),
+    ROLE_SUPER_ADMIN(SUPER_ADMIN_AUTHORITIES);
+
+    private final String[] authorities;
+
+    Role(String... authorities) {
+        this.authorities = authorities;
+    }
+
+    public String[] getAuthorities() {
+        return authorities;
+    }
+}
