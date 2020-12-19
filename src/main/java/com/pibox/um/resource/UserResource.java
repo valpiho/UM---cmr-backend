c00ae13a314f24bbf6ec3f1f5525e339b5e3877f
Valentin Piho <val.piho@gmail.com>
2020-12-19 17:42:44 +0200

    adding login functionality

--- src/main/java/com/pibox/um/resource/UserResource.java
+++ src/main/java/com/pibox/um/resource/UserResource.java
@@ -1,28 +1,58 @@
 package com.pibox.um.resource;
 
 import com.pibox.um.domain.User;
+import com.pibox.um.domain.UserPrincipal;
 import com.pibox.um.exception.ExceptionHandling;
 import com.pibox.um.exception.domain.EmailExistException;
 import com.pibox.um.exception.domain.UserNotFoundException;
 import com.pibox.um.exception.domain.UsernameExistException;
 import com.pibox.um.service.UserService;
+import com.pibox.um.utility.JWTTokenProvider;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
+
+import static com.pibox.um.constant.SecurityConstant.JWT_TOKEN_HEADER;
 
 @RestController
 @RequestMapping({"/user", "/"})
 public class UserResource extends ExceptionHandling {
 
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final JWTTokenProvider jwtTokenProvider;
 
-    public UserResource(UserService userService) {
+    public UserResource(UserService userService, AuthenticationManager authenticationManager, JWTTokenProvider jwtTokenProvider) {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @PostMapping("/register")
     public ResponseEntity<User> register(@RequestBody User user) throws UserNotFoundException, UsernameExistException, EmailExistException {
         User newUser = userService.register(user.getFirstName(), user.getLastName(), user.getUsername(), user.getEmail());
         return new ResponseEntity<>(newUser, HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody User user) {
+        authenticate(user.getUsername(), user.getPassword());
+        User loginUser = userService.findUserByUsername(user.getUsername());
+        UserPrincipal userPrincipal = new UserPrincipal(loginUser);
+        HttpHeaders jwtHeader = getJwtHeader(userPrincipal);
+        return new ResponseEntity<>(loginUser, jwtHeader, HttpStatus.OK);
+    }
+
+    private void authenticate(String username, String password) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+    }
+
+    private HttpHeaders getJwtHeader(UserPrincipal userPrincipal) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(JWT_TOKEN_HEADER, jwtTokenProvider.generateJwtToken(userPrincipal));
+        return headers;
     }
 }
