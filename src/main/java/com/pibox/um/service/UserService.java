1e7d4fd44b306107e8da5562454a33ab821a2199
Valentin Piho <val.piho@gmail.com>
2020-12-24 10:07:33 +0200

    Implementing more services

--- src/main/java/com/pibox/um/service/UserService.java
+++ src/main/java/com/pibox/um/service/UserService.java
@@ -4,17 +4,31 @@ import com.pibox.um.domain.User;
 import com.pibox.um.exception.domain.EmailExistException;
 import com.pibox.um.exception.domain.UserNotFoundException;
 import com.pibox.um.exception.domain.UsernameExistException;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
 import java.util.List;
 
 public interface UserService {
 
-    User register(String firstName, String lastName, String username, String email) throws UserNotFoundException, UsernameExistException, EmailExistException, MessagingException;
+    User register(String firstName, String lastName, String username, String email)
+            throws UserNotFoundException, UsernameExistException, EmailExistException, MessagingException;
 
     List<User> getUsers();
 
     User findUserByUsername(String username);
 
     User findUserByEmail(String email);
+
+    User addNewUser(String firstName, String lastName, String username, String email,
+                    String role, boolean isNonLocked, boolean isActive, MultipartFile profileImage);
+
+    User updateUser(String currentUsername, String newFirstName, String newLastName, String newUsername, String newEmail,
+                    String role, boolean isNonLocked, boolean isActive, MultipartFile profileImage);
+
+    void deleteUser(Long id);
+
+    void resetPassword(String email);
+
+    User updateProfileImage(String username, MultipartFile profileImage);
 }
